dca3761cdd34682f9ee28e9cc33b72dd2218b92b
Filipe <fbianchi.andrade@gmail.com>
2025-01-06 15:58:04 -0300

    testando correção em teste

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=postgres user=root password=root dbname=root port=5432 sslmode=disable"
+	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Fatal("Erro ao conectar com o banco de dados: ", err)
